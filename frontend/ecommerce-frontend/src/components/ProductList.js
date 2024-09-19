4bb80598d58275662f6b52803b3329aac65a48d1
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-09-20 00:20:09 +0600

    Update product list.

--- frontend/ecommerce-frontend/src/components/ProductList.js
+++ frontend/ecommerce-frontend/src/components/ProductList.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./style/ProductList.css"; 
+import "./style/ProductList.css";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     axios.get("http://localhost:8000/api/products/products").then((res) => {
@@ -12,9 +13,26 @@ function ProductList() {
     });
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % products.length);
+    }, 3000); // Change slide every 3 seconds
+    return () => clearInterval(interval);
+  }, [products]);
+
   return (
     <div className="product-list">
+      {products.length > 0 && (
+        <div className="slideshow">
+          <img
+            src={products[currentSlide].image}
+            alt={products[currentSlide].name}
+            className="slideshow-image"
+          />
+        </div>
+      )}
       <h1>All Products</h1>
+
       <div className="card-container">
         {products.map((product) => (
           <div className="product-card" key={product.id}>
