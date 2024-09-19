a848596d534525f46559a6d665f2e39e2ef05c28
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-09-19 23:45:48 +0600

    Edit navber option

--- frontend/ecommerce-frontend/src/components/Navbar.js
+++ frontend/ecommerce-frontend/src/components/Navbar.js
@@ -4,7 +4,14 @@ import "./style/Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <a href="/">Homepage</a>
+      <a href="/">
+        <img
+          src="/assets/images/01.png"
+          alt="Homepage"
+          className="navbar-logo"
+        />
+        Homepage
+      </a>
       <a href="#">Squid & Octopus</a>
       <a href="#">Frozen Fishes</a>
       <a href="#">Shrimps & Lobsters</a>
